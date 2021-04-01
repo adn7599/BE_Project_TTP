eeadb24a34817fe009741c81ed265d66fc38fba5
Advait Naik <advaitnaik7599@gmail.com>
2021-04-01 11:47:04 +0530

    Modified Relay's getUserCredentials
    Added custom status code 213 for user not registered at TTP

--- Routers/relay.js
+++ Routers/relay.js
@@ -2,57 +2,65 @@ const express = require("express");
 const conn = require("../Database");
 const router = express.Router();
 
-const verifyRelay = require('../Authentication/relayAuth')
-const config = require('../configuration.json')
+const verifyRelay = require("../Authentication/relayAuth");
+const config = require("../configuration.json");
 
-router.get("/userCredentials/:role/:reg_id",verifyRelay, (req, res,next) => {
-  if (req.params.role && req.params.reg_id) {
-    let role = req.params.role;
-    let reg_id = req.params.reg_id;
+router.get("/userCredentials/:role/:reg_id", verifyRelay, (req, res, next) => {
+  try {
+    if (req.params.role && req.params.reg_id) {
+      let role = req.params.role;
+      let reg_id = req.params.reg_id;
 
-    let tableName;
-    let colName = "Reg_no";
+      let tableName;
+      let colName = "Reg_no";
 
-    switch (role) {
-      case "customer":
-        tableName = "Customer";
-        colName = "Ration_no";
-        break;
-      case "SP":
-        tableName = "Supplier";
-        break;
-      case "DA":
-        tableName = "Distributor";
-        break;
-      default:
-        res
-          .status(400)
-          .json({ error: "Invalid Role, must be either customer or SP or DA" });
-        break;
-    }
-
-    let query = "SELECT Relay_Password FROM ?? WHERE ?? = ?";
-    let fquery = conn.format(query, [tableName, colName, reg_id]);
-
-    conn.query(fquery, (err, result, field) => {
-      if (err) {
-        next(err);
-      } else {
-        if (result.length == 0) {
-            res.status(404).json({ error : "Invalid User"});
-        } else {
-            let relayPassword = result[0].Relay_Password;
-            
-            res.json({
-                role,
-                reg_id,
-                relay_password: relayPassword,
-            });
-        }
+      switch (role) {
+        case "customer":
+          tableName = "Customer";
+          colName = "Ration_no";
+          break;
+        case "SP":
+          tableName = "Supplier";
+          break;
+        case "DA":
+          tableName = "Distributor";
+          break;
+        default:
+          res.status(400).json({
+            error: "Invalid Role, must be either customer or SP or DA",
+          });
+          break;
       }
-    });
-  } else {
-    res.status(400).json({ error: "URL Parameters role and reg_id required" });
+
+      let query = "SELECT Relay_Password FROM ?? WHERE ?? = ?";
+      let fquery = conn.format(query, [tableName, colName, reg_id]);
+
+      conn.query(fquery, (err, result, field) => {
+        if (err) {
+          next(err);
+        } else {
+          if (result.length == 0) {
+            res
+              .status(213) //custom status code
+              .json({ error: "User not registered here or invalid" });
+          } else {
+            let relayPassword = result[0].Relay_Password;
+
+            res.json({
+              role,
+              reg_id,
+              relay_password: relayPassword,
+            });
+          }
+        }
+      });
+    } else {
+      res
+        .status(400)
+        .json({ error: "URL Parameters role and reg_id required" });
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
